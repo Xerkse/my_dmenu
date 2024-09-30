218911d363c38a60c5290f152aceb66709c5c720
Xerkse <cjmp9876@gmail.com>
2024-09-30 13:09:17 -0500

    things

--- config.h
+++ config.h
@@ -4,10 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mononoki:size=15",
-	"NotoColorEmoji:pixelsize=20:antialias=true:autohint=true"
+	//"mononoki:size=15",
+	//"NotoColorEmoji:pixelsize=20:antialias=true:autohint=true"
 	//"JoyPixels:pixelsize=10:antialias=true:autohint=true"
+	"LiterationMonoNerdFont:pixelsize=15:antialias=true:autohint=true",
+	"NotoColorEmoji:pixelsize=20:antialias=true:autohint=true"
 };
+
 static const unsigned int bgalpha = OPAQUE;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
